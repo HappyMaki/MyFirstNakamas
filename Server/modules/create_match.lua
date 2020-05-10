4536d8a18093500fb0201c4b7677597a3ec3ebc0
Brandon Ling <brandonlingatl@gmail.com>
2020-05-10 15:54:48 -0400

    added constraint to prevent multiple rooms spawning

--- Server/modules/create_match.lua
+++ Server/modules/create_match.lua
@@ -1,14 +1,24 @@
 local nk = require("nakama")
+local match_info = require("match_info")
 
 local function create_match(context, payload)
   local decoded = nk.json_decode(payload)
-  
+
+  local limit = 1000
+  local authoritative = nil 
+  local min_size = 0
+  local max_size = 1000
+  local matches = nk.match_list(limit, authoritative, label, min_size, max_size)
+
+  if #matches ~= 0 then
+    return "nil"
+  end
+
   local modulename = decoded.modulename
-  -- local setupstate = { initialstate = decoded.label }
   local setupstate = { initialstate = decoded }
   local matchid = nk.match_create(modulename, setupstate)
 
-  -- Send notification of some kind
   return matchid
 end
+
 nk.register_rpc(create_match, "create_match_rpc")
--- Server/modules/match_info.lua
+++ Server/modules/match_info.lua
@@ -1,5 +1,7 @@
 local nk = require("nakama")
-local function get_match_list(context, payload)
+
+-- RPC Call to get match ID by label
+local function get_match_id(context, payload)
   local decoded = nk.json_decode(payload)
   local limit = 1000
   local authoritative = nil 
@@ -10,7 +12,6 @@ local function get_match_list(context, payload)
   
   -- design choice. only one match is allowed per label. for now anyway.
   assert(#matches <= 1)
-
   if #matches == 0 then
     return "Error: " .. label .. "does not exist in match list"
   end
@@ -20,4 +21,5 @@ local function get_match_list(context, payload)
     return m.match_id
   end
 end
-nk.register_rpc(get_match_list, "get_match_list_rpc")
+nk.register_rpc(get_match_id, "get_match_id_rpc")
+
